41b17554eee912014c98704b531a555fd596a4fc
Matt Ryan <matt@mvryan.org>
2014-08-25 16:50:00 -0600

    Add string parser for SDPBandwidth

--- sdp/src/main/java/com/zoomulus/speakeasy/sdp/messages/SDPBandwidth.java
+++ sdp/src/main/java/com/zoomulus/speakeasy/sdp/messages/SDPBandwidth.java
@@ -1,5 +1,7 @@
 package com.zoomulus.speakeasy.sdp.messages;
 
+import java.util.Optional;
+
 import lombok.Getter;
 import lombok.Value;
 import lombok.experimental.Accessors;
@@ -23,5 +25,25 @@ public class SDPBandwidth
     public String toString()
     {
         return String.format("%s:%d", bwtype, bandwidth);
+    }
+
+    public static Optional<SDPBandwidth> fromString(final String stringrep)
+    {
+        Optional<SDPBandwidth> result = Optional.<SDPBandwidth> empty();
+        final String[] parts = stringrep.split(":");
+        if (parts.length == 2)
+        {
+            try
+            {
+                final SDPBandwidthType bwtype = SDPBandwidthType
+                        .valueOf(parts[0]);
+                final int bandwidth = Integer.valueOf(parts[1]);
+                result = Optional.of(new SDPBandwidth(bwtype, bandwidth));
+            }
+            catch (final IllegalArgumentException e)
+            {
+            }
+        }
+        return result;
     }
 }
--- sdp/src/test/java/com/zoomulus/speakeasy/sdp/messages/TestSDPBandwidth.java
+++ sdp/src/test/java/com/zoomulus/speakeasy/sdp/messages/TestSDPBandwidth.java
@@ -1,6 +1,9 @@
 package com.zoomulus.speakeasy.sdp.messages;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
 
 import org.junit.Test;
 
@@ -13,8 +16,23 @@ public class TestSDPBandwidth
     public void testToString()
     {
         final SDPBandwidth bw = new SDPBandwidth(SDPBandwidthType.CT, 128);
-        System.out.println(bw.toString());
         assertTrue("CT:128".equals(bw.toString()));
+    }
+
+    @Test
+    public void testParseValid()
+    {
+        final Optional<SDPBandwidth> bw = SDPBandwidth.fromString("CT:128");
+        assertTrue("CT:128".equals(bw.get().toString()));
+    }
+
+    @Test
+    public void testParseInvalid()
+    {
+        final Optional<SDPBandwidth> bw = SDPBandwidth.fromString("BOB:128");
+        assertFalse(bw.isPresent());
+        final Optional<SDPBandwidth> bw2 = SDPBandwidth.fromString("CT:BILL");
+        assertFalse(bw2.isPresent());
     }
 
 }
